83f27fefd1a84b33d4554d11a5fcefe9081fdc9a
kai.chen <kai.chen@relaistech.net>
2022-06-08 12:08:36 +0800

    feat: add RestaurantInfoModal

---
+++ src/components/RestaurantInfoModal.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { Popup, Button } from 'vant'
+import { ref, computed } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const props = defineProps({
+  store: Object,
+  isShow: Boolean,
+})
+
+const infoData = computed(() => props.store)
+
+const isShow = computed(() => props.isShow)
+
+const back = () => {
+  // todo: 點擊回到【派工單 –初始進入】
+  // router.push({
+  //    path: `/`,
+  // })
+}
+</script>
+
+<template>
+  <Popup class="rounded-[20px] w-[90%] md:w-[300px]" v-model:show="isShow">
+    <div class="py-[35px] px-[28px] flex flex-col items-stretch">
+      <div class="text-[16px] text-gray text-center">餐廳資訊</div>
+      <div class="flex gap-1 text-primary font-bold text-[15px] my-5">
+        <div class="basis-[10%] p-1 bg-[#f2f2f2] rounded-l-lg">{{ infoData.bu }}</div>
+        <div class="basis-[45%] p-1 text-center bg-[#f2f2f2]">{{ infoData.number }}</div>
+        <div class="basis-[45%] p-1 text-center bg-[#f2f2f2] rounded-r-lg">{{ infoData.name }}</div>
+      </div>
+      <div class="flex items-center mb-2">
+        <img class="w-[28px] mr-1" src="/dispatching_map.png" alt="icon" />
+        <div class="text-[12px] grow text-gray bg-[#f2f2f2] p-3">
+          {{ infoData.address }}
+        </div>
+      </div>
+      <div class="flex items-center mb-5">
+        <img class="w-[28px] mr-1" src="/dispatching_phone.png" alt="icon" />
+        <div class="text-[13px] grow text-gray w-100 text-center p-3 bg-[#f2f2f2]">{{ infoData.tel }}</div>
+      </div>
+      <Button class="px-[50px] border-gray text-gray rounded-full border-2" @click="back()">返回</Button>
+    </div>
+  </Popup>
+</template>
