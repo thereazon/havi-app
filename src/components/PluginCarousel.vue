39b8c955bb685dd757d75ff3aec08a13dd8ccccb
Abe Chen <46858104+abe1272001@users.noreply.github.com>
2022-06-09 08:42:27 +0800

    feat: plugin work table and carousel (#9)
    * feat: PluginWorkItem ui finish
    
    * feat: finish PluginCarousel

---
+++ src/components/PluginCarousel.vue
@@ -0,0 +1,98 @@
+<script setup>
+import { reactive } from 'vue'
+import Flicking from '@egjs/vue3-flicking'
+import { Pagination } from '@egjs/flicking-plugins'
+import '@egjs/vue3-flicking/dist/flicking.css'
+import '@egjs/flicking-plugins/dist/pagination.css'
+import PluginWorkItem from './PluginWorkItem.vue'
+
+const plugins = [new Pagination({ type: 'bullet' })]
+const works = reactive([
+  {
+    id: 'FO12345',
+    no: 'INS112320NDC1840A1',
+    date: '01/19/2021',
+    time: '17:00',
+    bu: 'MD',
+    number: '00000001',
+    name: '中山店',
+    address: '台北市中山路',
+    tel: '02-2222-4444',
+    note: '工作說明',
+    status: 0,
+  },
+  {
+    id: 'FO12346',
+    no: 'INS112320NDC1840A1',
+    date: '01/19/2021',
+    time: '17:00',
+    bu: 'MD',
+    number: '00000001',
+    name: '中山店',
+    address: '台北市中山路',
+    tel: '02-2222-4444',
+    note: '工作說明',
+    status: 0,
+  },
+  {
+    id: 'FO12347',
+    no: 'INS112320NDC1840A1',
+    date: '01/19/2021',
+    time: '17:00',
+    bu: 'MD',
+    number: '00000001',
+    name: '中山店',
+    address: '台北市中山路',
+    tel: '02-2222-4444',
+    note: '工作說明',
+    status: 0,
+  },
+  {
+    id: 'FO12322',
+    no: 'INS112320NDC1840A1',
+    date: '01/19/2021',
+    time: '17:00',
+    bu: 'MD',
+    number: '00000001',
+    name: '中山店',
+    address: '台北市中山路',
+    tel: '02-2222-4444',
+    note: '工作說明',
+    status: 0,
+  },
+  {
+    id: 'FO12311',
+    no: 'INS112320NDC1840A1',
+    date: '01/19/2021',
+    time: '17:00',
+    bu: 'MD',
+    number: '00000001',
+    name: '中山店',
+    address: '台北市中山路',
+    tel: '02-2222-4444',
+    note: '工作說明',
+    status: 0,
+  },
+])
+</script>
+
+<template>
+  <div class="w-full h-56 pt-3">
+    <div class="text-center mb-3.5 text-[15px] text-success">插件工作</div>
+    <Flicking :options="{ circular: true, align: 'center' }" :plugins="plugins" class="h-44">
+      <div v-for="item in works" :key="item.id">
+        <PluginWorkItem :no="item.no" :date="item.date" :time="item.time" :address="item.address" />
+      </div>
+      <template #viewport>
+        <div class="flicking-pagination"></div>
+      </template>
+    </Flicking>
+  </div>
+</template>
+
+<style scoped>
+/* 無法改動 pagination active 的顏色 */
+/* .flicking-pagination .flicking-pagination-bullet-active {
+  background-color: #6dbe5b !important;
+} */
+</style>
---
+++ src/components/PluginWorkItem.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { Button, Icon } from 'vant'
+defineProps({
+  no: String,
+  date: String,
+  time: String,
+  address: String,
+})
+</script>
+
+<template>
+  <div class="border-solid border-success border-2 rounded-2xl h-[139px] w-[308px] mx-1">
+    <div class="bottom-dashed flex py-2 px-4 justify-between">
+      <div class="flex justify-center items-center space-x-2 text-success text-[13px]">
+        <label>單號</label>
+        <span>{{ no }}</span>
+      </div>
+      <Button class="px-[20px] text-[15px]" color="#6dbe5b" size="small">開始配送</Button>
+    </div>
+    <div class="px-4 flex justify-between mt-3">
+      <div class="flex items-center space-x-1">
+        <img class="w-[18px]" src="/dispatching_calendar.png" alt="" />
+        <div class="w-[80px] text-[12px] text-center text-gray bg-[#f2f2f2] p-1">{{ date }}</div>
+      </div>
+      <div class="flex items-center space-x-1">
+        <img class="w-[18px]" src="/dispatching_clock.png" alt="" />
+        <div class="w-[43px] text-[12px] text-center text-gray bg-[#f2f2f2] p-1">{{ time }}</div>
+      </div>
+      <a class="text-[13px] text-success" href="">
+        詳細資訊
+        <Icon name="play" />
+      </a>
+    </div>
+    <div class="flex items-center justify-between space-x-1 px-4 mt-3">
+      <img class="w-[28px]" src="/dispatching_map_2.png" alt="" />
+      <div class="w-[233px] text-[12px] text-success font-bold bg-[#f2f2f2] py-1 px-2">
+        {{ address }}
+        <a class="float-right text-success font-bold" href="">...</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.bottom-dashed {
+  border-bottom: 2px dashed #6dbe5b;
+}
+</style>
