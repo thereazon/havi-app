90aef836a4f0e496b45ad64bbbfd30e5a2464099
changray <raychang@glinteagle.com>
2022-06-10 00:04:45 +0800

    feat: signature function taken out to common component

--- src/components/CounterSignature.vue
+++ src/components/CounterSignature.vue
@@ -1,56 +1,15 @@
 <script setup>
-import { onMounted, reactive, ref } from 'vue'
+import { reactive, ref } from 'vue'
 import { currentTempData, TempForm } from '@/utils/mock'
 import { Checkbox, CheckboxGroup } from 'vant'
+import SignatureComponent from './SignatureComponent.vue'
 
 const checked = ref([])
-const moving = ref(false)
-const ctx = ref(null)
-const point = reactive({
-	x: 0,
-	y: 0
+const isCelsiusTemp = ref(false)
+const state = reactive({
+  freezing: '',
+  refrigeration: ''
 })
-
-onMounted(() => {
-	ctx.value = document.getElementById("canvas").getContext("2d")
-	ctx.value.strokeStyle = "#000"
-})
-const touchStart = (e) => {
-	let elem = document.querySelector('canvas')
-	let rect = elem.getBoundingClientRect()
-	let x = e.touches[0].clientX - rect.left
-	let y = e.touches[0].clientY - rect.top
-	point.x = x
-	point.y = y
-	ctx.value.beginPath()
-	ctx.value.lineWidth = 1
-  ctx.value.shadowBlur = 1
-	moving.value = true
-}
-const touchMove = (e) => {
-	if (moving.value) {
-		let elem = document.querySelector('canvas')
-		let rect = elem.getBoundingClientRect();
-		let x = e.touches[0].clientX - rect.left
-		let	y = e.touches[0].clientY - rect.top
-		ctx.value.moveTo(point.x, point.y)
-		ctx.value.lineTo(x, y)
-		ctx.value.stroke()
-		point.x = x 
-		point.y = y
-	}
-}
-const touchEnd = () => {
-	if (moving.value) {
-		ctx.value.closePath()
-		moving.value = false
-	}
-}
-const clearcanvas = () => {
-  let canvas = document.getElementById("canvas")
-  ctx.value.setTransform(1, 0, 0, 1, 0, 0);
-  ctx.value.clearRect(0, 0, canvas.width, canvas.height);
-}
 </script>
 
 <template>
@@ -83,9 +42,18 @@ const clearcanvas = () => {
 
     <div class="mt-[37px] mb-4 flex justify-between items-center">
       <span class="text-primary text-[0.9375rem]">填寫溫度</span>
-      <div class="w-[30%] h-[14px] text-[0.75rem] rounded-md bg-primary border-[3px] border-solid border-primary flex items-center">
-        <span class="w-[50%] h-full text-white flex justify-center items-center">°C</span>
-        <span class="w-[50%] h-full text-primary flex justify-center items-center bg-white rounded-r-md">°F</span>
+      <div
+        class="w-[30%] h-[14px] text-[0.75rem] rounded-md bg-primary border-[3px] border-solid border-primary flex items-center"
+        @click="isCelsiusTemp = !isCelsiusTemp"
+      >
+        <span
+          class="w-[50%] h-full flex justify-center items-center"
+          :class="[isCelsiusTemp ? 'text-white' : 'text-primary bg-white rounded-l-md']"
+        >°C</span>
+        <span
+          class="w-[50%] h-full flex justify-center items-center"
+          :class="[isCelsiusTemp ? 'text-primary bg-white rounded-r-md' : 'text-white']"
+        >°F</span>
       </div>
     </div>
     <div
@@ -94,7 +62,10 @@ const clearcanvas = () => {
       class="w-full h-[42px] rounded-xl shadow-md bg-white flex justify-evenly items-center text-[0.75rem] font-bold mb-2.5 last:mb-0"
     >
       <span class="w-[16%] text-primary">{{item.title}}</span>
-      <input class="w-[60%] h-[24px] py-0 px-1 border-0 text-center bg-[#f2f2f2] rounded-md text-[#242424]"/>
+      <input
+        class="w-[60%] h-[24px] py-0 px-1 border-0 text-center bg-[#f2f2f2] rounded-md text-[#242424]"
+        v-model="state[item.key]"
+      />
     </div>
     <div class="w-full h-[42px] rounded-xl shadow-md bg-white flex items-center text-[0.75rem] font-bold">
       <CheckboxGroup v-model="checked" direction="horizontal">
@@ -102,39 +73,18 @@ const clearcanvas = () => {
         <Checkbox shape="square" name="b">車廂清潔確認</Checkbox>
       </CheckboxGroup>
     </div>
-		
-		<div class="mt-8 mb-[5px] text-[#959595] flex items-center justify-center relative">
-			<div class="text-center text-[0.8125rem] font-bold">櫃台簽名</div>
-			<div
-        class="w-[16%] absolute top-0 right-0 text-center text-[#eb5e55] text-[0.75rem] border-1 border-solid border-[#eb5e55] rounded-full"
-        @click="clearcanvas()"
-      >
-        清除
-      </div>
-		</div>
 
-		 <div 
-			class="divide w-full h-28 border-1 border-solid border-transparent"
-			@touchmove.prevent
-		>
-      <canvas
-        id="canvas"
-        class="w-full h-full"
-        @touchstart="touchStart"
-        @touchmove="touchMove"
-        @touchend="touchEnd"
-      ></canvas>
+		<SignatureComponent />
+
+    <div
+      class="h-11 mt-7 text-white font-bold text-[1.0625rem] flex justify-center items-center bg-success rounded-full"
+    >
+      完成
     </div>
-
-    <div class="h-11 mt-7 text-white font-bold text-[1.0625rem] flex justify-center items-center bg-success rounded-full">完成</div>
   </div>
 </template>
 
 <style scoped>
-.divide {
-  background: linear-gradient(#fffcf6, #fffcf6) padding-box,
-  repeating-linear-gradient(-45deg, #707070 0, #707070 0.6rem, #fffcf6 0, #fffcf6 1rem)
-}
 :deep(.van-checkbox__label) {
   color: #086eb6 !important;
   font-size: 0.75rem;
