237d74ecb59159f8cf1b217e909b79e9b531fc06
CalvinChang <q20274982@gmail.com>
2022-06-11 10:40:25 +0800

    feat(ContainerOnloadList script): add props

--- src/components/ContainerOnloadRecord/components/ContainerOnloadList.vue
+++ src/components/ContainerOnloadRecord/components/ContainerOnloadList.vue
@@ -1,5 +1,12 @@
 <script setup>
 import { Cell, CellGroup } from 'vant'
+
+const props = defineProps({
+  'ingredientsList': {
+    type: Array,
+    required: true
+  }
+})
 </script>
 
 <template>
@@ -7,16 +14,15 @@ import { Cell, CellGroup } from 'vant'
     <p class="text-xs text-center py-1.5 my-0 text-primary font-bold">請於下方填寫容器裝車記錄數值</p>
     <CellGroup>
       <Cell
-        center
-        title="5加崙糖漿桶"
-        value="0"
+        v-for="ingredient in props.ingredientsList"
         class="border-0 border-t border-solid border-neutral-300 "
         title-class="text-primary text-sm font-bold pb-3"
         value-class="text-teal-400 text-sm font-bold"
+        center
+        :key="ingredient.id"
+        :value="ingredient.value"
+        :title="ingredient.title"
       />
     </CellGroup>
   </div>
 </template>
-
-<style lang="scss" scoped>
-</style>
