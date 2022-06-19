20d58fa1600e14ae3e595edd4fbe1ceb29493a54
shihkaihung <prodigygod0209@gmail.com>
2022-06-19 17:11:36 +0800

    fix: dispatch service

--- src/views/Containers/service/index.js
+++ src/views/Containers/service/index.js
@@ -1,16 +1,6 @@
 import axios from 'axios'
 
 const getContainers = async (id, fleet_id, car_id, container_id) => {
-<<<<<<< HEAD
-  return await axios
-    .get(`/api/container`, {
-      data: {
-        id, // 司機id
-        fleet_id, //車隊ID
-        car_id, //車號ID
-        container_id, //櫃號ID
-      },
-=======
   const params = {
     id, // 司機id
     fleet_id, //車隊ID
@@ -21,7 +11,6 @@ const getContainers = async (id, fleet_id, car_id, container_id) => {
   return await axios
     .get(`/api/container`, {
       params,
->>>>>>> main
     })
     .then((res) => res.data)
     .catch((err) => Promise.reject(err.response.data))
