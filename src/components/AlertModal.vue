7635367e8bfcbd3e668f30ca841b696739770673
grasdira <69340316+gras-chen@users.noreply.github.com>
2022-06-20 11:31:49 +0800

    fix: flexible modal style

--- src/components/AlertModal.vue
+++ src/components/AlertModal.vue
@@ -7,19 +7,42 @@ const VanDialog = Dialog.Component
 const emit = defineEmits(['update:show'])
 const props = defineProps({
   show: Boolean,
-  error: Boolean,
+  modalType: String,
 })
 const isShow = computed({
   get: () => props.show,
 })
+
+const styleDefine = {
+  success: {
+    header: 'bg-green-400',
+    button: 'text-green-400 border-green-400',
+    iconName: 'passed',
+  },
+  hint: {
+    header: 'bg-blue-400',
+    button: 'text-blue-400 border-blue-400',
+    iconName: 'info-o',
+  },
+  warning: {
+    header: 'bg-orange-400',
+    button: 'text-orange-400 border-orange-400',
+    iconName: 'warning-o',
+  },
+  error: {
+    header: 'bg-red-400',
+    button: 'text-red-400 border-red-400',
+    iconName: 'close',
+  },
+}
 </script>
 
 <template>
   <div>
     <van-dialog v-model:show="isShow" class="rounded-md w-3/4">
       <template v-slot:title>
-        <div class="text-center text-white p-[24px]" :class="[error ? 'bg-red-400' : 'bg-blue-400']">
-          <van-icon class="text-[2.5rem]" :name="error ? 'close' : 'info-o'" />
+        <div class="text-center text-white p-[24px]" :class="styleDefine[modalType]['header']">
+          <van-icon class="text-[2.5rem]" :name="styleDefine[modalType]['iconName']" />
         </div>
       </template>
 
@@ -35,7 +58,7 @@ const isShow = computed({
         <div class="bg-white flex py-[8px] px-[16px]">
           <button
             class="flex-auto bg-transparent text-[{theme}]-400 py-2 px-4 border rounded-md border-[{theme}]-400"
-            :class="error ? 'text-red-400 border-red-400' : 'text-blue-400 border-blue-400'"
+            :class="styleDefine[modalType]['button']"
             @click="emit('update:show', false)"
           >
             OK
@@ -57,36 +80,21 @@ const isShow = computed({
   ### script ###
   import AlertModal from '@/components/AlertModal.vue'
 
-  const isAlertModal = ref(false)
-  const isErrorDialog = ref(false)
-  const showAlertModal = () => {
-    isAlertModal.value = true
-  }
-  const showErrorDialog = () => {
-    isErrorDialog.value = true
+  # 變數命名可自訂
+  const isHintModal = ref(false)
+  const showHintModal = () => {
+    isHintModal.value = true
   }
 
   ### template ###
-  <Button @click="showAlertModal"> 提示</Button>
-  <Button @click="showErrorDialog"> 錯誤</Button>
+  <Button @click="showHintModal"> 提示</Button>
 
-  # error === false 藍色
-  <AlertModal v-model:show="isAlertModal" :error="false">
+  <AlertModal v-model:show="isHintModal" modalType="hint">
     <template v-slot:titleContent>
       <div>提示(自訂標題)</div>
     </template>
     <template v-slot:content>
-      <div>請先完成報到，才能進行預冷溫度(自訂內容)</div>
-    </template>
-  </AlertModal>
-
-  # error === true 紅色
-  <AlertModal v-model:show="isErrorDialog" :error="true">
-    <template v-slot:titleContent>
-      <div>錯誤(自訂標題)</div>
-    </template>
-    <template v-slot:content>
-      <div>沒有派工單須配送(自訂內容)</div>
+      <div>提示(自訂內容)</div>
     </template>
   </AlertModal>
  -->
