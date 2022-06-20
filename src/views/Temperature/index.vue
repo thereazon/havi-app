38702a1b37d3c5bcd0a5f9a12414e1543fe7b87f
kaikai0517 <86772522+kaikai0517@users.noreply.github.com>
2022-06-20 09:00:36 +0800

    feat: add temperature view (#28)
    Co-authored-by: kai.chen <kai.chen@relaistech.net>

--- src/views/Temperature/index.vue
+++ src/views/Temperature/index.vue
@@ -1,27 +1,87 @@
 <script setup>
 import { onMounted } from 'vue'
-import useAccountInfo from '@/views/Login/store'
 import useTempInfo from '@/views/Temperature/store'
-import { useRoute } from 'vue-router'
-const accountStore = useAccountInfo()
+import { useRoute, useRouter } from 'vue-router'
 const tempStore = useTempInfo()
-const router = useRoute()
-
-console.log(router.query.container_id)
-console.log(accountStore.account.fleet_id)
-// 請用這兩組id去獲取溫度資訊
+const route = useRoute()
+const router = useRouter()
 
 onMounted(() => {
-  tempStore.getTemperatureAction(router.query.container_id) // 需要先判斷container_id是否存在，若不存在請把它彈回車次選擇頁面 /cars
+  if (!route.query.container_id) {
+    router.push({ path: '/car' })
+  } else {
+    tempStore.getTemperatureAction(route.query.car_id, route.query.container_id)
+  }
 })
 </script>
 
-//todo ui
 <template>
-  <div>車號</div>
-  <div>{{ accountStore.account.fleet }}</div>
-  <div>櫃號</div>
-  <div>{{ accountStore.account.fleet }}</div>
+  <div class="flex flex-col items-stretch bg-primary bg-opacity-[0.05] px-[40px]">
+    <div class="flex items-center mb-[60px] mt-[50px] ml-5">
+      <div class="mr-[35px]">
+        <img src="/login_car.png" alt="icon" />
+      </div>
+      <div class="text-[22px]">
+        <div class="mb-[25px]">
+          <div>車號</div>
+          <div class="text-[#828282]">123</div>
+        </div>
+        <div>
+          <div>櫃號</div>
+          <div class="text-[#828282]">123</div>
+        </div>
+      </div>
+    </div>
 
-  <div>溫度區塊</div>
+    <div>
+      <div class="flex items-center bg-white p-5 rounded-[20px] shadow-[0_2px_2px_0_rgba(0,0,0,0.16)] mb-3">
+        <div>
+          <img class="w-[40px] h-[40px] mr-5" src="/temperature_freezer_icon1.png" alt="icon" />
+        </div>
+        <div class="w-full">
+          <div class="text-primary font-bold mb-2">冷凍品溫</div>
+          <div class="grid grid-cols-2 gap-2">
+            <div class="bg-[#f3f3f3] text-center rounded py-1">{{ tempStore.temperature?.c.frozen }}</div>
+            <div class="bg-[#f3f3f3] text-center rounded py-1">{{ tempStore.temperature?.f.frozen }}</div>
+          </div>
+        </div>
+      </div>
+      <div class="flex items-center bg-[#f3f3f3] p-5 rounded-[20px] shadow-[0_2px_2px_0_rgba(0,0,0,0.16)] mb-3">
+        <div>
+          <img class="w-[40px] h-[40px] mr-5" src="/temperature_refrigerator_icon1.png" alt="icon" />
+        </div>
+        <div class="w-full">
+          <div class="text-[#858585] font-bold mb-2">冷藏品溫</div>
+          <div class="grid grid-cols-2 gap-2">
+            <div class="bg-white text-center rounded py-1">{{ tempStore.temperature?.c.cold }}</div>
+            <div class="bg-white text-center rounded py-1">{{ tempStore.temperature?.f.cold }}</div>
+          </div>
+        </div>
+      </div>
+      <div class="flex items-center bg-white p-5 rounded-[20px] shadow-[0_2px_2px_0_rgba(0,0,0,0.16)] mb-3">
+        <div>
+          <img class="w-[40px] h-[40px] mr-5" src="/temperature_freezer_icon2.png" alt="icon" />
+        </div>
+        <div class="w-full">
+          <div class="text-primary font-bold mb-2">冷凍廂溫</div>
+          <div class="grid grid-cols-2 gap-2">
+            <div class="bg-[#f3f3f3] text-center rounded py-1">{{ tempStore.temperature?.c.frozen_storage }}</div>
+            <div class="bg-[#f3f3f3] text-center rounded py-1">{{ tempStore.temperature?.f.frozen_storage }}</div>
+          </div>
+        </div>
+      </div>
+      <div class="flex items-center bg-[#f3f3f3] p-5 rounded-[20px] shadow-[0_2px_2px_0_rgba(0,0,0,0.16)] mb-3">
+        <div>
+          <img class="w-[40px] h-[40px] mr-5" src="/temperature_refrigerator_icon2.png" alt="icon" />
+        </div>
+        <div class="w-full">
+          <div class="text-[#858585] font-bold mb-2">冷藏廂溫</div>
+          <div class="grid grid-cols-2 gap-2">
+            <div class="bg-white text-center rounded py-1">{{ tempStore.temperature?.c.cold_storage }}</div>
+            <div class="bg-white text-center rounded py-1">{{ tempStore.temperature?.f.cold_storage }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
--- src/views/Temperature/store/index.js
+++ src/views/Temperature/store/index.js
@@ -1,6 +1,5 @@
 import { defineStore } from 'pinia'
 import TempApiCaller from '@/views/Temperature/service'
-import useAccountInfo from '@/views/Login/store'
 
 const useTempInfo = defineStore('temperature', {
   state: () => ({
@@ -10,11 +9,10 @@ const useTempInfo = defineStore('temperature', {
     message: '',
   }),
   actions: {
-    async getTemperatureAction(containerId) {
-      const accountStore = useAccountInfo()
+    async getTemperatureAction(carId, containerId) {
       this.isLoading = true
       try {
-        const response = await TempApiCaller.getTemperature(accountStore.account.fleet_id, containerId)
+        const response = await TempApiCaller.getTemperature(carId, containerId)
         if (response.status === 'success') {
           this.temperature = response.data
           this.status = response.status
