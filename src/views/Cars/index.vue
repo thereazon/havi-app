86c3cad3b7107cca2cbf3689c98a618ba8009095
shihkaihung <prodigygod0209@gmail.com>
2022-06-25 14:15:49 +0800

    fix: cars page bg

--- src/views/Cars/index.vue
+++ src/views/Cars/index.vue
@@ -56,52 +56,52 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="bg-primary bg-opacity-[0.05] flex flex-col items-stretch px-[50px]">
-    <div class="mt-[75px] m-auto"><img src="/login_car.png" alt="icon" /></div>
-    <div class="mt-[20px]">
-      <div class="text-gray">您的車隊</div>
-      <Cell value-class="text-[#828282] text-[22px]" class="bg-white border-solid border-gray mt-2 mb-[67px]">{{
-        accountStore.account.fleet
-      }}</Cell>
-    </div>
-    <div>
-      <div class="text-gray mb-2">選擇車號</div>
+  <div class="bg-primary bg-opacity-[0.05] h-[100vh]">
+    <div class="flex flex-col items-stretch px-[50px]">
+      <div class="mt-[75px] m-auto"><img src="/login_car.png" alt="icon" /></div>
+      <div class="mt-[20px]">
+        <div class="text-gray">您的車隊</div>
+        <Cell value-class="text-[#828282] text-[22px]" class="bg-white border-solid border-gray mt-2 mb-[67px]">{{
+          accountStore.account.fleet
+        }}</Cell>
+      </div>
+      <div>
+        <div class="text-gray mb-2">選擇車號</div>
 
-      <select v-model="selectCarId" class="w-full border-dashed p-5 bg-[#fffcf6] mb-4" name="選擇車號" id="">
-        <option v-for="car in carsStore.cars" :key="car.id" :value="car.id">{{ car.number }}</option>
-      </select>
-    </div>
-    <div>
-      <div class="text-gray mb-2">選擇櫃號</div>
-      <select v-model="selectContainerId" class="w-full border-dashed p-5 bg-[#fffcf6]" name="選擇櫃號" id="">
-        <option v-for="container in carsStore.containers" :key="container.id" :value="container.id">
-          {{ container.number }}
-        </option>
-      </select>
-    </div>
-    <div class="text-warning mt-3 text-center" v-html="errorMessage"></div>
+        <select v-model="selectCarId" class="w-full border-dashed p-5 bg-[#fffcf6] mb-4" name="選擇車號" id="">
+          <option v-for="car in carsStore.cars" :key="car.id" :value="car.id">{{ car.number }}</option>
+        </select>
+      </div>
+      <div>
+        <div class="text-gray mb-2">選擇櫃號</div>
+        <select v-model="selectContainerId" class="w-full border-dashed p-5 bg-[#fffcf6]" name="選擇櫃號" id="">
+          <option v-for="container in carsStore.containers" :key="container.id" :value="container.id">
+            {{ container.number }}
+          </option>
+        </select>
+      </div>
+      <div class="text-warning mt-3 text-center" v-html="errorMessage"></div>
 
-    <Button
-      loading-type="spinner"
-      class="mt-[70px] bg-success text-white"
-      round
-      block
-      native-type="submit"
-      @click="comfirmDispatch"
-    >
-      完成</Button
-    >
+      <Button
+        loading-type="spinner"
+        class="mt-[70px] bg-success text-white"
+        round
+        block
+        native-type="submit"
+        @click="comfirmDispatch"
+      >
+        完成</Button
+      >
 
-    <Button
-      loading-type="spinner"
-      class="my-[40px] bg-[#6e6e6f] text-white"
-      round
-      block
-      native-type="submit"
-      @click="getContainers"
-      >查看容器對點單</Button
-    >
+      <Button
+        loading-type="spinner"
+        class="my-[40px] bg-[#6e6e6f] text-white"
+        round
+        block
+        native-type="submit"
+        @click="getContainers"
+        >查看容器對點單</Button
+      >
+    </div>
   </div>
-  <!-- 按鈕顏色記得換下 -->
-  <!-- 請把容器id帶在url上跳到容器頁面 eg http://localhost:3000/containers?id="CC6033764f892b5" -->
 </template>
