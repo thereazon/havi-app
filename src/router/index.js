f9f767eaac1bf55d725f4ec8822ece32112dd088
CalvinChang <q20274982@gmail.com>
2022-06-29 09:34:26 +0800

    feat(router): add rotuer guard judge user login behavior

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import useAccountInfo from '@/views/Login/store'
 import Login from '@/views/Login/index.vue'
 import Cars from '@/views/Cars/index.vue'
 import Temperature from '@/views/Temperature/index.vue'
@@ -12,10 +13,12 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      name: 'Login',
       path: '/',
       component: Login,
     },
     {
+      name: 'Cars',
       path: '/cars',
       component: Cars,
     },
@@ -24,11 +27,13 @@ const router = createRouter({
       component: Main,
       children: [
         {
+          name: 'Temperature',
           path: 'temperature',
           component: Temperature,
           alias: '/temperature',
         },
         {
+          name: 'Dispatch',
           path: 'dispatch',
           component: Dispatch,
           alias: '/dispatch',
@@ -36,18 +41,32 @@ const router = createRouter({
       ],
     },
     {
+      name: 'Containers',
       path: '/containers',
       component: Containers,
     },
     {
+      name: 'EntryRecord',
       path: '/entryRecord',
       component: EntryRecord,
     },
     {
+      name: 'Restaurant',
       path: '/restaurant',
       component: Restaurant,
     },
   ],
+})
+
+router.beforeEach(async (from, to) => {
+  const accountStore = useAccountInfo()
+  const isAccountStored = !!accountStore.account
+
+  if (!isAccountStored) return { name: 'Login' }
+
+  if (to.name === 'Login' && isAccountStored) {
+    return { name: 'Dispatch' }
+  }
 })
 
 export default router
