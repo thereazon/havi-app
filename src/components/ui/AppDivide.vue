79273313a2fd683dbdb382334ff50e6997189230
CalvinChang <q20274982@gmail.com>
2022-07-06 09:20:25 +0800

    feat: add ui/AppDivide

---
+++ src/components/ui/AppDivide.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="divide py-1"></div>
+</template>
+
+<style lang="scss" scoped>
+.divide {
+  width: 100%;
+  height: 1px;
+  background-image: linear-gradient(to right, #086eb6 0%, #086eb6 70%, transparent 50%);
+  background-size: 30px 2px;
+  background-repeat: repeat-x;
+}
+</style>
