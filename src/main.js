2907f85d6a2e14be656f3f4753f156ccf7bac7cf
shihkaihung <prodigygod0209@gmail.com>
2022-09-23 17:18:01 +0800

    feat: float

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import '@/styles/tailwind.css'
+import 'floating-vue/dist/style.css'
 import { Icon } from 'vant'
+import FloatingVue from 'floating-vue'
 
 const app = createApp(App)
 app.use(createPinia())
+app.use(FloatingVue)
 app.use(Icon)
 app.use(router)
 app.mount('#app')
